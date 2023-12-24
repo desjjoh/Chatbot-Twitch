35ac90751fda9b03af8e6aec734ac3e1db75f9ae
John Desjardins <desjjoh@gmail.com>
2023-12-24 14:09:33 -0500

    Switch was a pain to update

--- utils/commands.constants.js
+++ utils/commands.constants.js
@@ -1,24 +1,27 @@
 import { apiClient } from '../plugins/twurple.js'
-import { DatabaseService } from '../services/database.js'
-
-import { dehash } from './formatter.js'
 
 const commands = {
-  GAME: {
-    command: 'game',
-    event: async (payload) => {
-      const { tags, $command, channel } = payload
-      const { username } = tags
+  game: async (payload) => {
+    const { tags, $command, channel } = payload
+    const [raw, command, argument] = $command
+    const { username } = tags
 
-      const [raw, command, argument] = $command
+    const { CHANNEL_NAME } = process.env
 
-      //   const USER = await apiClient.users.getUserByName(channel)
-      //   const STREAM = await apiClient.streams.getStreamByUserName(dehash(channel))
-      //   const CHANNEL = await apiClient.channels.getChannelInfoById(USER.id)
+    const STREAM = await apiClient.streams.getStreamByUserName(CHANNEL_NAME)
+    const game = STREAM?.gameName
 
-      const message = `Hey @${username}. `
-      await sendChat({ channel: CHANNEL_NAME, message })
+    if (!game) {
+      await sendChat({
+        channel: CHANNEL_NAME,
+        message: ''
+      })
     }
+
+    await sendChat({
+      channel: CHANNEL_NAME,
+      message: `Hey @${username}.`
+    })
   }
 }
 
--- utils/commands.js
+++ utils/commands.js
@@ -3,27 +3,29 @@ import { sendChat } from '../plugins/tmi.js'
 import { commands } from './commands.constants.js'
 
 async function chatCommand(payload) {
-  const { message, channel, tags } = payload
+  const { message, tags } = payload
   const { username } = tags
+
+  const { CHANNEL_NAME } = process.env
 
   const regExpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/)
   const [raw, command, argument] = message.match(regExpCommand)
 
-  const data = { ...payload, $command: [raw, command, argument] }
-
   const $message = `info: [${channel}] <${username}>: ${message}`
   logger.add({ $message })
 
-  switch (command) {
-    case commands.GAME.command:
-      await commands.GAME.event(data)
-      break
-    default:
-      await sendChat(
-        channel,
-        `Sorry @${username}. Your request could not be completed. Reason: !${command} is not initialized.`
-      )
+  const event = commands[command]
+
+  if (!event) {
+    await sendChat({
+      channel: CHANNEL_NAME,
+      message: `Sorry @${username}. Your request could not be completed. Reason: !${command} is not initialized.`
+    })
+    return
   }
+
+  const data = { ...payload, $command: [raw, command, argument] }
+  await event(data)
 }
 
 export { chatCommand }
